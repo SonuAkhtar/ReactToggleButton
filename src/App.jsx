309e82ba95ee53ab57e038ebdedf7f103dead6fb
SonuA <sonua981@gmail.com>
2024-06-08 21:51:04 +0530

    created toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
   return (
     <>
-      <div>Hello</div>
+      <div className={`container ${isDark && "dark"}`}>
+        <div className="wrapper" onClick={() => setIsDark(!isDark)} />
+      </div>
     </>
   );
 }
